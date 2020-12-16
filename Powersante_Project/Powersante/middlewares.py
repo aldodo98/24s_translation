07b69fa7b2f5043ab716246b55d095b5f762fabb
Tianwen XIE <tianwen.xie@tianconsulting.fr>
2020-12-16 23:58:09 +0100

    complete chrom options

--- Powersante_Project/Powersante/middlewares.py
+++ Powersante_Project/Powersante/middlewares.py
@@ -18,8 +18,10 @@ from itemadapter import is_item, ItemAdapter
 
 
 chrome_options=Options()
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 # from selenium.webdriver.chrome.options import Options
 # useful for handling different item types with a single interface
 
