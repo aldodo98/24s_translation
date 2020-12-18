d76fb485ab94c4da1ca843d3b77fd8cada190da3
Tianwen XIE <tianwen.xie@tianconsulting.fr>
2020-12-18 20:25:01 +0100

    correct allowed_domains

--- Matchfashion_Project/Maje/spiders/RootTaskSpider.py
+++ Matchfashion_Project/Maje/spiders/RootTaskSpider.py
@@ -139,7 +139,7 @@ from string import Template
 class DiorSpider(RedisSpider):
     redis_key = BOT_NAME + ':RootTaskSpider'
     name = 'RootTaskSpider'
-    allowed_domains = ['https://fr.maje.com']
+    allowed_domains = ['fr.maje.com']
     main_url = 'https://fr.maje.com'
 
     # __init__方法必须按规定写，使用时只需要修改super()里的类名参数即可
