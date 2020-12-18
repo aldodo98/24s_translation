ffeaa1969600c9770a32cda820f4ed33382fe6f0
Tianwen XIE <tianwen.xie@tianconsulting.fr>
2020-12-18 15:27:08 +0100

    update config haiwa admin

--- haiwa-scrapy-admin/src/config/env.js
+++ haiwa-scrapy-admin/src/config/env.js
@@ -1,6 +1,6 @@
 const configs = {
   production: {
-    WEB_API_SERVICES: 'https://haiwa-crawl-funcs-labs.azurewebsites.net',
+    WEB_API_SERVICES: 'https://haiwa-crawl-func-lab-westeu.azurewebsites.net',
     Scrapyd_Web_Url: 'http://137.116.216.95:5555/'
   }
 }
